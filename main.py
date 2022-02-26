7471658a474ca9b1e173af5b42016ef32558bf9c
rishf <65766216+rishf@users.noreply.github.com>
2022-02-26 13:33:25 +0000

    song volumes

--- main.py
+++ main.py
@@ -8,13 +8,14 @@ CORRECT_HIT_TOLERANCE = 0.01
 
 
 global_timer = 0
+in_beat_window = False
 # last_time, running
 
 MILLIS_EVT = pygame.USEREVENT + 1
 
 
 def handleUI(events):
-    global running, global_timer, last_time
+    global running, global_timer, last_time, in_beat_window
     for event in events:
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -22,22 +23,30 @@ def handleUI(events):
 
         elif event.type == pygame.KEYDOWN:
             pygame.mixer.Sound("./res/hat.mp3").play()
-            delta = global_timer - time_per_beat
-            if abs(delta) < CORRECT_HIT_TOLERANCE:
-                print(f"On time {delta}")
-                global_timer -= time_per_beat
+            if in_beat_window:
+                print(f"On time {global_timer}")
             else:
-                print(f"Early {delta}")
+                print(f"Miss {global_timer}")
 
         elif event.type == MILLIS_EVT:
             now = time.time()
+            prev_timer = global_timer
             global_timer += now - last_time
             last_time = now
 
-            if global_timer > time_per_beat + CORRECT_HIT_TOLERANCE:
-                print(f"Missed beat {global_timer}")
-                global_timer -= time_per_beat
+            def just_crossed_time(prev, cur, time):
+                return prev < time and cur >= time
+            if just_crossed_time(prev_timer, global_timer, time_per_beat / 2 - CORRECT_HIT_TOLERANCE):
+                in_beat_window = True
+                print("Beat start")
+            if just_crossed_time(prev_timer, global_timer, time_per_beat / 2):
+                print("BEAT")
+            if just_crossed_time(prev_timer, global_timer, time_per_beat / 2 + CORRECT_HIT_TOLERANCE):
+                in_beat_window = False
+                print("Beat end")
 
+            if global_timer >= time_per_beat:
+                global_timer -= time_per_beat
 
 def startMusic():
     pygame.mixer.init()
@@ -45,7 +54,6 @@ def startMusic():
     pygame.mixer.music.play(loops=-1)
     # pygame.mixer.music.set_volume(0.7)
     pygame.mixer.music.play()
-    print('a')
 
 def main():
     global running, last_time
