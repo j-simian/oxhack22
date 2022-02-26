1482bb2b6b7a12acdeb69ba7d7bfa7bbde5734ef
Antek <github@tmp.pegazy.org>
2022-02-26 23:34:32 +0000

    convert squares to beatmap

--- src/beatmap.py
+++ src/beatmap.py
@@ -1,28 +1,47 @@
+import math
 import json
 
 class Beatmap:
     def __init__(self, file):
+        self.len = 0
+        self.times = []
+        self.angles = []
+        self.angles_abs = []
+        self.pos = []
         mapjson = json.loads(open(file, "r").read())
         mapbpm=float(mapjson["bpm"])
+        self.bpm = mapbpm
         offset=float(mapjson["offset"])
+        self.offset = offset
         beatTimes=[]
         beatAngles=[]
         for each in mapjson["beats"]:
             beatFraction=each[0]
             if len(beatFraction)==1:
-                beatTimes.append(offset+(60/mapbpm)*int(beatFraction[0]))
+                time = offset+(60/mapbpm)*int(beatFraction[0])
             else:
-                beatTimes.append(offset+(60/mapbpm)*(int(beatFraction[0])+int(beatFraction[1])/int(beatFraction[2])))
-            beatAngles.append(each[1])
+                time = offset+(60/mapbpm)*(int(beatFraction[0])+int(beatFraction[1])/int(beatFraction[2]))
+            angle = each[1]
+            self.add(time, angle, False)
 
-        cumAngles = [0]
-        for a in beatAngles:
-            cumAngles.append((cumAngles[-1] + a) % 360)
 
-        self.len = len(beatTimes)
-        assert len(beatTimes) == len(beatAngles)
-        self.times = beatTimes
-        self.angles = beatAngles
-        self.angles_abs = cumAngles[1:]
-        self.bpm = mapbpm
-        self.offset = offset
+    def add(self, time, angle, abs):
+        self.len += 1
+
+        prev_abs_angle = self.angles_abs[-1] if self.angles_abs else 0
+
+        dt = time - self.times[-1] if self.times else time
+        dx = dt * math.cos(prev_abs_angle * math.pi / 180)
+        dy = dt * math.sin(prev_abs_angle * math.pi / 180)
+        if self.pos:
+            self.pos.append((self.pos[-1][0] + dx, self.pos[-1][1] + dy))
+        else:
+            self.pos.append((dx, dy))
+        self.times.append(time)
+
+        if abs:
+            self.angles.append((angle - prev_abs_angle) % 360)
+            self.angles_abs.append(angle)
+        else:
+            self.angles.append(angle)
+            self.angles_abs.append((prev_abs_angle + angle) % 360)
