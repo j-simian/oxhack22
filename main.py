062998ffe760f11c19b1faac19b3aa0ab40490c4
Jake Simian <jxssica.simian@gmail.com>
2022-02-26 12:43:25 +0000

    Moved graphics to gfx.py

--- main.py
+++ main.py
@@ -1,26 +1,12 @@
 import pygame 
+import gfx
 from time import time
 
-screen_width = 1920/2
-screen_height = 1080/2
 bpm = 128
 time_per_beat = 60 / bpm
 # running, start_time, beat_count
 
 
-def initGfx():
-    global screen
-    pygame.init()
-    screen = pygame.display.set_mode((screen_width, screen_height))
-    pygame.display.set_caption("Hackathon rhythm game")
-    clearScreen()
-    swapBuffers()
-    
-def clearScreen():
-    screen.fill((0, 0, 0))
-
-def swapBuffers():
-    pygame.display.flip()
 
 def handleUI(events):
     global running, beat_count
@@ -39,7 +25,7 @@ def handleUI(events):
 def main():
     global running, start_time, beat_count
     print("Hello world")
-    initGfx()
+    gfx.initGfx()
     running = True
     start_time = time()
     beat_count = 0
