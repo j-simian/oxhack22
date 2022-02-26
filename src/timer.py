00d7cf58e8e6b99b720a4cb5defaa17aa8159154
Antek <github@tmp.pegazy.org>
2022-02-26 19:44:11 +0000

    direction check

--- src/timer.py
+++ src/timer.py
@@ -5,7 +5,8 @@ PERFECT_TOLERANCE_FRAC = 0.2
 
 class Timer:
     def __init__(self, beatmap):
-        self.beatmap = deque(sorted(beatmap.times))
+        self.beatmap = beatmap
+        self.current_beat = 0
         self.hit_this_beat = False
         self.miss_last_beat = False
 
@@ -18,23 +19,23 @@ class Timer:
         def just_crossed_time(prev, cur, time):
             return prev < time and cur >= time
 
-        if len(self.beatmap) == 0:
+        if self.current_beat == self.beatmap.len:
             return
 
-        beat_time = self.beatmap[0]
+        beat_time = self.beatmap.times[self.current_beat]
         beat_tol = self._current_beat_tolerance_oneway()
         if just_crossed_time(prev_timer, self.global_timer, beat_time):
             print("BEAT")
 
         if self.global_timer >= beat_time + beat_tol:
-            self.beatmap.popleft()
+            self.current_beat += 1
             self.miss_last_beat = not self.hit_this_beat
             self.hit_this_beat = False
 
     # Returns the score for this hit.
     # Handles double-hit on the same beat as a fail.
-    def register_hit(self):
-        if self.hit_this_beat:
+    def register_hit(self, dir):
+        if not self.is_valid_hit(dir):
             return -1000
         self.hit_this_beat = True
 
@@ -53,21 +54,23 @@ class Timer:
     
     def _current_beat_tolerance_oneway(self):
         # FIXME Consider distance to previous note as well, not just the next one.
-        if len(self.beatmap) >= 2:
-            return min(MAX_TOLERANCE / 2, (self.beatmap[1] - self.beatmap[0]) / 2)
+        if self.current_beat + 1 < self.beatmap.len:
+            return min(MAX_TOLERANCE / 2, (self.beatmap.times[self.current_beat+1] - self.beatmap.times[self.current_beat]) / 2)
         else:
             return MAX_TOLERANCE / 2
 
     def is_in_beat_window(self):
         return abs(self.delta()) < 1
-    def is_valid_hit(self):
-        return self.is_in_beat_window() and not self.hit_this_beat
+    def is_valid_hit(self, dir):
+        return (self.is_in_beat_window() and not self.hit_this_beat
+            and self.current_beat < self.beatmap.len 
+            and self.beatmap.angles[self.current_beat] == dir)
 
     # Returns offset from perfect beat in range [-1, 1]
     # If not currently in beat window, don't rely on this output.
     def delta(self):
-        if len(self.beatmap) > 0:
+        if self.current_beat < self.beatmap.len:
             tol = self._current_beat_tolerance_oneway()
-            return (self.global_timer - self.beatmap[0]) / tol
+            return (self.global_timer - self.beatmap.times[self.current_beat]) / tol
         else:
             return -100
