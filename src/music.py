70df520221f2dc4c97ff713a0a92e2d9070ea61f
Jake Simian <jxssica.simian@gmail.com>
2022-02-26 18:58:16 +0000

    made song less bad

--- src/music.py
+++ src/music.py
@@ -3,7 +3,7 @@ import pygame
 class Music:
     def start_music(self):
         pygame.mixer.init()
-        pygame.mixer.music.load("./res/song.mp3")
+        pygame.mixer.music.load("./res/song.wav")
         pygame.mixer.music.play(loops=-1)
         # pygame.mixer.music.set_volume(0.7)
         pygame.mixer.music.play()
