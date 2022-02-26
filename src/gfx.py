801df0209536adb9f6cd3a20ed0ecde7e3f9bed5
rishf <65766216+rishf@users.noreply.github.com>
2022-02-26 16:55:48 +0000

    Update gfx.py

--- src/gfx.py
+++ src/gfx.py
@@ -5,6 +5,9 @@ from pygame.locals import *
 SCREEN_WIDTH = 1920/2
 SCREEN_HEIGHT = 1080/2
 
+ERROR_WIDTH = SCREEN_WIDTH/2 - 100
+ERROR_HEIGHT = SCREEN_HEIGHT - 100
+
 COLOURS = [
         Color("#2E3440"), Color("#3B4252"), Color("#434C5E"), Color("#4C566A"), # DARK GREY COLOURS, INCREASING BRIGHTNESS
         Color("#D8DEE9"), Color("#E5E9F0"), Color("#ECEFF4"), # WHITE COLOURS, INCREASING BRIGHTNESS
@@ -39,22 +42,25 @@ class Gfx:
         if self.timer.is_in_beat_window():
             self.deltas.append((self.timer.delta(), self.timer.global_timer))
         else:
-            self.deltas.append((-2, self.timer.global_timer))
+            self.deltas.append((-10, self.timer.global_timer))
 
         self.swapBuffers()
 
-    def render(self,score, board, delta):
-        self.clearScreen() 
-        board.render(self.screen, delta)
-        
-        pygame.draw.rect(self.screen, COLOURS[2], pygame.Rect(0,0,200,50))
+    def drawErrorTimer(self):
+        pygame.draw.rect(self.screen, COLOURS[2], pygame.Rect(ERROR_WIDTH,ERROR_HEIGHT,200,50))
         for i in range(5):
             timeDif = self.timer.global_timer - self.deltas[i][1]
             if timeDif <= self.existence:
                 h = ((self.existence-timeDif)*50)/self.existence
             else:
                 h = 0
-            pygame.draw.rect(self.screen, COLOURS[7], pygame.Rect(95*(self.deltas[i][0]+1),0,10,h))
+            pygame.draw.rect(self.screen, COLOURS[7], pygame.Rect(ERROR_WIDTH+  95*(self.deltas[i][0]+1),ERROR_HEIGHT,10,h))        
+
+    def render(self,score, board, delta):
+        self.clearScreen() 
+        board.render(self.screen, delta)
+        
+        self.drawErrorTimer()
 
         myfont = pygame.font.SysFont("Comic Sans MS", 30)
         img = myfont.render(f"score: {int(score)}", 1, COLOURS[3])
