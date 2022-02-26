bc75d58343c2affbe310f460f74f838efac1b5ec
Jake Simian <jxssica.simian@gmail.com>
2022-02-26 15:50:59 +0000

    board colours n shit

--- src/board.py
+++ src/board.py
@@ -8,6 +8,8 @@ SQUARE_OFFSET = 5
 
 class Board:
 
+    player = 0
+
     beats = [1] * 100
     squares = [[0, 0], [2, 1], [5, -1], [5, 0], [2, 1], [2, 1], [2, -1]]
 
@@ -16,7 +18,13 @@ class Board:
         y = gfx.SCREEN_HEIGHT/2
         angle = 0
         for i in self.squares:
-            pygame.draw.circle(screen, gfx.COLOURS[11], (x, y), SQUARE_SIZE)
+            currColour = gfx.COLOURS[4] 
+            if i[1] == 1:
+                currColour = gfx.COLOURS[11]
+            elif i[1] == -1:
+                currColour = gfx.COLOURS[7]
+
+            pygame.draw.circle(screen, currColour, (x, y), SQUARE_SIZE)
             angle += i[1]
             x += math.cos(angle*math.pi/2)*i[0] * (DIST)
             y += math.sin(angle*math.pi/2)*i[0] * (DIST)
