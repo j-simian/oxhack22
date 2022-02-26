154770da8342b7cc7756c36126464f8992f00efd
Antek <github@tmp.pegazy.org>
2022-02-26 12:34:33 +0000

    track offset from beat

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pygame 
+from time import time
 
 screen_width = 1920/2
 screen_height = 1080/2
+bpm = 128
+time_per_beat = 60 / bpm
+# running, start_time, beat_count
 
 
 def initGfx():
@@ -19,18 +23,26 @@ def swapBuffers():
     pygame.display.flip()
 
 def handleUI(events):
-    global running
+    global running, beat_count
     for event in events:
         if event.type == pygame.QUIT:
             pygame.quit()
             running = False
+        elif event.type == pygame.KEYDOWN:
+            # TODO adjust beat_count on missed beat
+            beat_count += 1
+            now = time()
+            exp = start_time + time_per_beat * beat_count
+            print(now - exp)
 
 
 def main():
-    global running
+    global running, start_time, beat_count
     print("Hello world")
     initGfx()
     running = True
+    start_time = time()
+    beat_count = 0
     while running:
         handleUI(pygame.event.get())
     print("Done")
