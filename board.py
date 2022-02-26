98413ed88521f169fbafbfda3898f264f92975e0
Jake Simian <jxssica.simian@gmail.com>
2022-02-26 15:00:57 +0000

    board

--- board.py
+++ board.py
@@ -1,16 +1,20 @@
 import pygame
 import gfx
 
-SQUARE_SIZE = 50
-OFFSET = 100
+SQUARE_SIZE = 60
+SQUARE_OFFSET = 5
 
 class Board:
 
-    squares = [
-            [0, 0], [1, 0], [2, 1], [3, 0]
-            ]
+    beats = [1] * 100
+    squares = []
+
+    def genSquares(self):
+        self.squares = []
+        for i in self.beats:
+            self.squares.append()
 
     def render(self, screen):
         for i in self.squares:
-            pygame.draw.rect(screen, gfx.COLOURS[11], pygame.Rect(OFFSET + SQUARE_SIZE*i[0], OFFSET + SQUARE_SIZE*i[1], SQUARE_SIZE, SQUARE_SIZE))
+            pygame.draw.rect(screen, gfx.COLOURS[11], pygame.Rect(gfx.SCREEN_WIDTH/2 + SQUARE_SIZE*i[0], gfx.SCREEN_HEIGHT/2 + SQUARE_SIZE*i[1], SQUARE_SIZE - SQUARE_OFFSET, SQUARE_SIZE - SQUARE_OFFSET))
 
