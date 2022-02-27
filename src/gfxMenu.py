6aabf4f0f7dd9a4fd9a92993cbc4aaae75a7d865
rishf <65766216+rishf@users.noreply.github.com>
2022-02-27 04:56:46 +0000

    Update gfxMenu.py

--- src/gfxMenu.py
+++ src/gfxMenu.py
@@ -41,6 +41,8 @@ class GfxMenu:
                     return 4
                 if event.button == 7:
                     return 5
+                if event.button == 4:
+                    return 6
             for i in range(len(msgs)):
                 if (SCREEN_WIDTH/2 - sizes[i][0]/2 <= mouse[0] <= SCREEN_WIDTH/2 + sizes[i][0]/2) and (20+70*i<=mouse[1]<=20+70*i+sizes[i][1]):
                     kolor[i] = 3
