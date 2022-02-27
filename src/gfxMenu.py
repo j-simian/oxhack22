528397e59cbd32527dbfdd0a6a22e65d3be93797
Antek <github@tmp.pegazy.org>
2022-02-27 01:35:34 +0000

    slight menu clearnup

--- src/gfxMenu.py
+++ src/gfxMenu.py
@@ -3,33 +3,9 @@ import math
 from collections import deque
 from pygame.locals import *
 from const import EDITOR_MODE
+from gfx import SCREEN_WIDTH, SCREEN_HEIGHT, COMPASSCOLOURS, COLOURS
 
-SCREEN_WIDTH = 1920/2
-SCREEN_HEIGHT = 1080/2
-
-kolor = [2,2]
-
-ERROR_WIDTH = SCREEN_WIDTH/2 - 100
-ERROR_HEIGHT = SCREEN_HEIGHT - 100
-
-COLOURS = [
-        Color("#2A2D34"), Color("#3B4252"), Color("#434C5E"), Color("#4C566A"), # DARK GREY COLOURS, INCREASING BRIGHTNESS
-        Color("#D8DEE9"), Color("#E5E9F0"), Color("#ECEFF4"), # WHITE COLOURS, INCREASING BRIGHTNESS
-        Color("#8FBCBB"), Color("#88C0D0"), Color("#81A1C1"), Color("#5E81AC"), # BLUE COLOURS, INCREASING BRIGHTNESS
-        Color("#BF616A"), Color("#D08770"), Color("#EBCB8B"), Color("#A3BE8C"), Color("#B48EAD") # RED, ORANGE, YELLOW, GREEN, PURPLE
-        ]
-
-COMPASSCOLOURS = [
-        Color("#FFFFFF"), #White
-        Color("#FF8800"), #orange
-        Color("#FF0000"), 
-        Color("#FF00FF"), 
-        Color("#0000FF"), 
-        Color("#00FFFF"), 
-        Color("#00FF00"), 
-        Color("#FFFF00") 
-]
-
+kolor = [2, 2]
 
 class GfxMenu:
     def __init__(self):
@@ -62,6 +38,7 @@ class GfxMenu:
             else:
                 kolor[1] = 2
         return 0
+
     def render(self):
         global kolor
         self.clearScreen() 
@@ -75,8 +52,4 @@ class GfxMenu:
         self.screen.blit(img, (SCREEN_WIDTH/2 - 140, 10))
         self.screen.blit(img2, (SCREEN_WIDTH/2 - 140, 80))
 
-
-
         self.swapBuffers()
-def lerp(a, b, t):
-    return ((a*t) + (b*(1-t)))
