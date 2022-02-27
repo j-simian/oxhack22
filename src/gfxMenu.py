b490adb41ec8b6a14fc5dd71143e7cb6a26caf45
rishf <65766216+rishf@users.noreply.github.com>
2022-02-27 01:20:46 +0000

    spid dance

--- src/gfxMenu.py
+++ src/gfxMenu.py
@@ -7,7 +7,7 @@ from const import EDITOR_MODE
 SCREEN_WIDTH = 1920/2
 SCREEN_HEIGHT = 1080/2
 
-kolor = 2
+kolor = [2,2]
 
 ERROR_WIDTH = SCREEN_WIDTH/2 - 100
 ERROR_HEIGHT = SCREEN_HEIGHT - 100
@@ -50,21 +50,30 @@ class GfxMenu:
         mouse = pygame.mouse.get_pos()
         for event in events:
             if (SCREEN_WIDTH/2 - 140 <= mouse[0] <= SCREEN_WIDTH/2 + 140) and (20<=mouse[1]<=70):
-                kolor = 3
+                kolor[0] = 3
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     return 1
             else:
-                kolor = 2
+                kolor[0] = 2
+            if (SCREEN_WIDTH/2 - 140 <= mouse[0] <= SCREEN_WIDTH/2 + 140) and (80<=mouse[1]<=130):
+                kolor[1] = 3
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    return 2
+            else:
+                kolor[1] = 2
         return 0
     def render(self):
         global kolor
         self.clearScreen() 
 
-        pygame.draw.rect(self.screen, COMPASSCOLOURS[kolor], pygame.Rect(SCREEN_WIDTH/2-140,20,260,50))
+        pygame.draw.rect(self.screen, COMPASSCOLOURS[kolor[0]], pygame.Rect(SCREEN_WIDTH/2-140,20,260,50))
+        pygame.draw.rect(self.screen, COMPASSCOLOURS[kolor[1]], pygame.Rect(SCREEN_WIDTH/2-140,90,260,50))
 
         myfont = pygame.font.SysFont("Comic Sans MS", 48)
         img = myfont.render("Just Dance", 1, COLOURS[6])
+        img2 = myfont.render("Spid Dance", 1, COLOURS[6])
         self.screen.blit(img, (SCREEN_WIDTH/2 - 140, 10))
+        self.screen.blit(img2, (SCREEN_WIDTH/2 - 140, 80))
 
 
 
